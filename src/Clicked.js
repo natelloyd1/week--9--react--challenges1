1dfed11ba76675c04a7c35378238a7444c007219
natelloyd1 <nathanaellloyd@live.com>
2020-03-03 17:18:49 +0000

    created clicked

---
+++ src/Clicked.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Clicked extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { clicked: false}; 
+
+        this.handleClick = this.handleClick.bind(this); 
+    }
+
+    handleClick() {
+        this.setState({ clicked:!this.state.clicked })
+    }
+    
+    render() {
+
+        let { clicked } = this.state; 
+            
+        return ( 
+                <p onClick={ this.handleClick }>
+                { clicked ? "clicked" : "non clicked"}
+                </p>
+        );  
+    }
+}
+
+export default Clicked;
