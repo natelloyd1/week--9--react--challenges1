ff2715ee8e2cc1af66082ae04943df27f1bd37e6
natelloyd1 <nathanaellloyd@live.com>
2020-03-05 14:36:41 +0000

    progess

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 // import the React library
 import React from "react";
 import Stuff from "./Stuff";
+import Header from "./Header";
+import People from "./People"; 
+import Clicked from "./Clicked";
+import Length from "./Length"; 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 
 // create our component: just a function that returns JSX
 
 const App = () => ( 
-
-  <Stuff square = {true} />
-
+  <Router>
+    <Switch>
+      <Route path="/header">
+        <Header text="Hello Props" exact component={Header} />
+      </Route> 
+      <Route path="/length" exact component={Length} /> 
+      <Route path="/people" exact component={People} />
+      <Route path="/allpeople">
+        <People names = {["James P. Sullivan", "Mike Wazowski", "Boo", "Randall Boggs", "Roz", "Fungus"]}/>
+      </Route>
+      <Route path="/clicked" exact component={Clicked} />
+      <Route path="/all" exact >
+        <Stuff square = {true}/>
+      </Route>
+    </Switch>
+  </Router>
   );  
 
 // export our component
